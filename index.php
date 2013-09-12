e9f87abcc2d5d5661e35c0d6ba84af79eb3fa57f
James Tang <jiangbo.tang@gmail.com>
2013-09-11 21:37:17 -0400

    fixed some English grammar errors

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 			My name is James (Jiangbo) Tang. I work full time as a researcher on breast and ovarian cancer at the University of Pennsylvania. Before I started at Penn, I obtained my Ph.D. from the University of Pittsburgh and BS from Wuhan University, China.
 		</p>
 		<p>
-			Enough of those boring stuff :). I like computer related stuff, particularly gaming (anyone doesn't?). I love biological science as well as CS. Which one I love more is an undecided question for me. But I do like the peace of mind when working with CS stuff, as most of the time if mess up something, I know there is a fix and normally I can get it fixed in hours (hmmm, maybe sometimes in days). In biological science, not so much, there is too much variables out of your control and it always bugs me. Darn, guess I may need to see a shrink about that. 
+			Enough of those boring stuff :). I like computer related stuff, particularly gaming (anyone doesn't?). I love biological science as well as CS. Which one I love more is an undecided question for me. But I do like the peace of mind when working with CS stuff, as most of the time if I mess up something, I know there is a fix and normally I can get it fixed in hours (hmmm, maybe sometimes in days). In biological science, not so much, there are too much variables that are out of your control, which always bugs me. Darn, guess I may need to see a shrink about that. 
 		</p>
 		<p>
 			All rights reserved and all materials edited in Sublime 2 :).
