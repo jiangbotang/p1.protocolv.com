3975df9bc958839068bf6af07457f0e6151b9804
James Tang <jiangbo.tang@gmail.com>
2013-09-11 21:27:57 -0400

    added name bio and editor info to the index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-Welcome to p1!
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Jiangbo Tang's bio</title>
+	</head>
+
+	<body>
+		<h1>Jiangbo Tang</h1>
+		<p>
+			Welcome my page, fellows!
+		</p>
+		<p>
+			My name is James (Jiangbo) Tang. I work full time as a researcher on breast and ovarian cancer at the University of Pennsylvania. Before I started at Penn, I obtained my Ph.D. from the University of Pittsburgh and BS from Wuhan University, China.
+		</p>
+		<p>
+			Enough of those boring stuff :). I like computer related stuff, particularly gaming (anyone doesn't?). I love biological science as well as CS. Which one I love more is an undecided question for me. But I do like the peace of mind when working with CS stuff, as most of the time if mess up something, I know there is a fix and normally I can get it fixed in hours (hmmm, maybe sometimes in days). In biological science, not so much, there is too much variables out of your control and it always bugs me. Darn, guess I may need to see a shrink about that. 
+		</p>
+		<p>
+			All rights reserved and all materials edited in Sublime 2 :).
+		</p>
+
+	</body>
+</html>
